700a3f81ed7dff934db0210a43752dc757d9688e
Abhisheknayak-programer <sahiservice300@gmail.com>
2026-07-25 09:03:08 +0530

    Day 10 Completed

---
+++ Day10/day10.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("titanic.csv")
+print(df.head())
+print(df.info())
+
+X = df[["Pclass", "Age", "Fare"]]
+y = df["Survived"]
+
+X["Age"] = X["Age"].fillna(X["Age"].mean())
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+
+print(f'Model Accurcy: {accuracy:.2%}')
